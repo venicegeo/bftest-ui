3b694214ea1a9be12c8462db473ae14dc5afefd2
Rahul Srivastava <RahulsIM@Elyses-MacBook-Pro.local>
2016-08-22 09:52:54 -0400

    BF UI login automated test script

---
+++ src/main/java/beachfront/ui/test/BeachFrontLoginTest.java
@@ -0,0 +1,80 @@
+package beachfront.ui.test;
+
+import static org.junit.Assert.fail;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BeachFrontLoginTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "http://localhost:8082/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testBeachFrontLoginTest() throws Exception {
+    driver.get(baseUrl + "/login#0:27674:110,0");
+    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+    System.out.println("After Launching Beach Front and logging in");
+    Thread.sleep(5000);
+    System.out.println("Closing Window");
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
